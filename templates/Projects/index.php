16aeb684d1d0a49cef7ac41d44cbda0af8a4241c
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-10-27 03:27:08 +1100

    feat: apply datatables correctly to projects index

--- templates/Projects/index.php
+++ templates/Projects/index.php
@@ -5,43 +5,45 @@
  * @var iterable<\App\Model\Entity\Project> $projects
  */
 ?>
-<div class="projects index content">
-    <?= $this->Html->link(__('New Project'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+<div class="projects index content p-5">
+    <?= $this->Html->link(__('Create Project'), ['action' => 'add'], ['class' => 'btn btn-secondary float-right']) ?>
     <h3><?= __('Projects') ?></h3>
-    <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Project Name</th>
-                    <th>Management Tool Link</th>
-                    <th>Due Date</th>
-                    <th>Last Checked</th>
-                    <th>Completed</th>
-                    <th>Contractor ID</th>
-                    <th>Organisation ID</th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($projects as $project): ?>
+    <div class="my-4">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
                     <tr>
-                        <td><?= $this->Number->format($project->id) ?></td>
-                        <td><?= h($project->project_name) ?></td>
-                        <td><?= h($project->management_tool_link) ?></td>
-                        <td><?= h($project->project_due_date) ?></td>
-                        <td><?= h($project->last_checked) ?></td>
-                        <td><?= h($project->complete) ?></td>
-                        <td><?= $project->hasValue('contractor') ? $this->Html->link($project->contractor->id, ['controller' => 'Contractors', 'action' => 'view', $project->contractor->id]) : '' ?></td>
-                        <td><?= $project->hasValue('organisation') ? $this->Html->link($project->organisation->id, ['controller' => 'Organisations', 'action' => 'view', $project->organisation->id]) : '' ?></td>
-                        <td class="actions">
-                            <?= $this->Html->link(__('View'), ['action' => 'view', $project->id]) ?>
-                            <?= $this->Html->link(__('Edit'), ['action' => 'edit', $project->id]) ?>
-                            <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $project->id], ['confirm' => __('Are you sure you want to delete # {0}?', $project->id)]) ?>
-                        </td>
+                        <th>ID</th>
+                        <th>Project Name</th>
+                        <th>Management Tool Link</th>
+                        <th>Due Date</th>
+                        <th>Last Checked</th>
+                        <th>Completed</th>
+                        <th>Contractor ID</th>
+                        <th>Organisation ID</th>
+                        <th class="actions"><?= __('Actions') ?></th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach ($projects as $project): ?>
+                        <tr>
+                            <td><?= $this->Number->format($project->id) ?></td>
+                            <td><?= h($project->project_name) ?></td>
+                            <td><?= h($project->management_tool_link) ?></td>
+                            <td><?= h($project->project_due_date) ?></td>
+                            <td><?= h($project->last_checked) ?></td>
+                            <td><?= h($project->complete) ?></td>
+                            <td><?= $project->hasValue('contractor') ? $this->Html->link($project->contractor->id, ['controller' => 'Contractors', 'action' => 'view', $project->contractor->id]) : '' ?></td>
+                            <td><?= $project->hasValue('organisation') ? $this->Html->link($project->organisation->id, ['controller' => 'Organisations', 'action' => 'view', $project->organisation->id]) : '' ?></td>
+                            <td class="actions">
+                                <?= $this->Html->link(__('View'), ['action' => 'view', $project->id]) ?>
+                                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $project->id]) ?>
+                                <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $project->id], ['confirm' => __('Are you sure you want to delete # {0}?', $project->id)]) ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
