cb78f14810f1b838619e722445e4ac6c70bdf093
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-10-28 08:41:43 +1100

    fix: datatable isn't broken, + org and contractor shows names instead of id

--- templates/Contacts/index.php
+++ templates/Contacts/index.php
@@ -13,13 +13,13 @@ $this->layout = 'admin';
         <table id="dataTable" class="table table-bordered" width="100%" cellspacing="0">
             <thead>
                 <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('first_name') ?></th>
-                    <th><?= $this->Paginator->sort('last_name') ?></th>
-                    <th><?= $this->Paginator->sort('email') ?></th>
-                    <th><?= $this->Paginator->sort('phone_number') ?></th>
-                    <th><?= $this->Paginator->sort('organisation_id') ?></th>
-                    <th><?= $this->Paginator->sort('contractor_id') ?></th>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Organisation</th>
+                    <th>Contractor</th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
@@ -31,8 +31,8 @@ $this->layout = 'admin';
                         <td><?= h($contact->last_name) ?></td>
                         <td><?= h($contact->email) ?></td>
                         <td><?= h($contact->phone_number) ?></td>
-                        <td><?= $contact->hasValue('organisation') ? $this->Html->link($contact->organisation->id, ['controller' => 'Organisations', 'action' => 'view', $contact->organisation->id]) : '' ?></td>
-                        <td><?= $contact->hasValue('contractor') ? $this->Html->link($contact->contractor->id, ['controller' => 'Contractors', 'action' => 'view', $contact->contractor->id]) : '' ?></td>
+                        <td><?= $contact->hasValue('organisation') ? $this->Html->link($contact->organisation->business_name, ['controller' => 'Organisations', 'action' => 'view', $contact->organisation->id]) : '' ?></td>
+                        <td><?= $contact->hasValue('contractor') ? $this->Html->link(($contact->contractor->first_name . " " . $contact->contractor->last_name), ['controller' => 'Contractors', 'action' => 'view', $contact->contractor->id]) : '' ?></td>
                         <td class="actions">
                             <?= $this->Html->link(__('View'), ['action' => 'view', $contact->id]) ?>
                             <?= $this->Html->link(__('Edit'), ['action' => 'edit', $contact->id]) ?>
@@ -42,15 +42,5 @@ $this->layout = 'admin';
                 <?php endforeach; ?>
             </tbody>
         </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
 </div>
