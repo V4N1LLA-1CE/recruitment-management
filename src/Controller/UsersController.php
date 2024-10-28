087586294b4d21b4fdc8b1308fea6f1010339ae3
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-10-28 22:23:23 +1100

    fix: leaving password blank in user edit now doesn't change password

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -68,14 +68,22 @@ class UsersController extends AppController
     {
         $user = $this->Users->get($id, contain: []);
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
+            $data = $this->request->getData();
+
+            // Remove password from data if it's empty (keep password before)
+            if (empty($data['password'])) {
+                unset($data['password']);
+            }
+
+            $user = $this->Users->patchEntity($user, $data);
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
+        // Clear password field before displaying form
+        $user->password = '';
         $this->set(compact('user'));
     }
 
