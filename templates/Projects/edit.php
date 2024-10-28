45cdc0f336bd6dfc2c5a4f21810dfd8e1760161e
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-10-28 16:19:17 +1100

    feat: better styling for view and edit project

--- templates/Projects/edit.php
+++ templates/Projects/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Project $project
@@ -6,37 +7,31 @@
  * @var string[]|\Cake\Collection\CollectionInterface $organisations
  * @var string[]|\Cake\Collection\CollectionInterface $skills
  */
+$this->setLayout('admin')
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $project->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $project->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Projects'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="projects form content">
-            <?= $this->Form->create($project) ?>
+<div class="d-flex justify-content-center align-items-center">
+    <div class="fixed-width-600">
+        <div class="projects form">
+            <?= $this->Form->create($project, ['class' => 'd-flex flex-column gap-3']) ?>
             <fieldset>
-                <legend><?= __('Edit Project') ?></legend>
+                <h3 class="bold"><?= __('Edit Project') ?></h3>
                 <?php
-                    echo $this->Form->control('project_name');
-                    echo $this->Form->control('description');
-                    echo $this->Form->control('management_tool_link');
-                    echo $this->Form->control('project_due_date', ['empty' => true]);
-                    echo $this->Form->control('last_checked', ['empty' => true]);
-                    echo $this->Form->control('complete');
-                    echo $this->Form->control('contractor_id', ['options' => $contractors, 'empty' => true]);
-                    echo $this->Form->control('organisation_id', ['options' => $organisations, 'empty' => true]);
-                    echo $this->Form->control('skills._ids', ['options' => $skills]);
+                echo $this->Form->control('project_name', ['class' => 'form-control form-label']);
+                echo $this->Form->control('description', ['class' => 'form-control form-label']);
+                echo $this->Form->control('management_tool_link', ['class' => 'form-control form-label']);
+                echo $this->Form->control('project_due_date', ['class' => 'form-control form-label']);
+                echo $this->Form->control('last_checked', ['class' => 'form-control form-label']);
+                echo $this->Form->control('complete', ['type' => 'checkbox', 'label' => 'Completed?', 'class' => 'mt-4 mb-2']);
+                echo $this->Form->control('contractor_id', ['options' => $contractors, 'empty' => 'Select contractor', 'class' => 'w-100 form-control form-label']);
+                echo $this->Form->control('organisation_id', ['options' => $organisations, 'empty' => 'Select organisation', 'class' => 'w-100 form-control form-label']);
+                echo $this->Form->control('skills._ids', ['options' => $skills, 'class' => 'w-100 form-control form-label']);
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <div class="d-flex gap-2 mt-3 mb-5">
+                <?= $this->Form->button(__('Submit'), [
+                    'class' => 'btn btn-primary w-100',
+                ]) ?>
+            </div>
             <?= $this->Form->end() ?>
         </div>
     </div>
--- templates/Projects/view.php
+++ templates/Projects/view.php
@@ -1,87 +1,95 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Project $project
  */
+$this->setLayout('admin')
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Project'), ['action' => 'edit', $project->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Project'), ['action' => 'delete', $project->id], ['confirm' => __('Are you sure you want to delete # {0}?', $project->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Projects'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Project'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="projects view content">
-            <h3><?= h($project->id) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Project Name') ?></th>
-                    <td><?= h($project->project_name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Management Tool Link') ?></th>
-                    <td><?= h($project->management_tool_link) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Contractor') ?></th>
-                    <td><?= $project->hasValue('contractor') ? $this->Html->link($project->contractor->id, ['controller' => 'Contractors', 'action' => 'view', $project->contractor->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Organisation') ?></th>
-                    <td><?= $project->hasValue('organisation') ? $this->Html->link($project->organisation->id, ['controller' => 'Organisations', 'action' => 'view', $project->organisation->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($project->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Project Due Date') ?></th>
-                    <td><?= h($project->project_due_date) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Last Checked') ?></th>
-                    <td><?= h($project->last_checked) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Complete') ?></th>
-                    <td><?= $project->complete ? __('Yes') : __('No'); ?></td>
-                </tr>
-            </table>
-            <div class="text">
-                <strong><?= __('Description') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($project->description)); ?>
-                </blockquote>
+<div class="container py-4">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3 d-flex justify-content-between align-items-center">
+            <h3 class="m-0 font-weight-bold text-primary"><?= h($project->project_name) ?></h3>
+            <div>
+                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $project->id], ['class' => 'btn btn-warning btn-sm me-2 px-4']) ?>
+                <?= $this->Form->postLink(
+                    __('Delete'),
+                    ['action' => 'delete', $project->id],
+                    ['confirm' => __('Are you sure you want to delete this project?'), 'class' => 'btn btn-danger btn-sm px-3']
+                ) ?>
             </div>
-            <div class="related">
-                <h4><?= __('Related Skills') ?></h4>
-                <?php if (!empty($project->skills)) : ?>
-                <div class="table-responsive">
-                    <table>
+        </div>
+        <div class="card-body">
+            <div class="row mb-4">
+                <div class="col-md-6">
+                    <table class="table">
                         <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Skill Name') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
+                            <th class="w-50"><?= __('Project Name') ?></th>
+                            <td><?= h($project->project_name) ?></td>
                         </tr>
-                        <?php foreach ($project->skills as $skill) : ?>
                         <tr>
-                            <td><?= h($skill->id) ?></td>
-                            <td><?= h($skill->skill_name) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Skills', 'action' => 'view', $skill->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Skills', 'action' => 'edit', $skill->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Skills', 'action' => 'delete', $skill->id], ['confirm' => __('Are you sure you want to delete # {0}?', $skill->id)]) ?>
-                            </td>
+                            <th><?= __('Management Tool Link') ?></th>
+                            <td><?= $project->management_tool_link ? $this->Html->link($project->management_tool_link, $project->management_tool_link, ['target' => '_blank']) : '-' ?></td>
                         </tr>
-                        <?php endforeach; ?>
+                        <tr>
+                            <th><?= __('Contractor') ?></th>
+                            <td><?= $project->hasValue('contractor') ? $this->Html->link($project->contractor->first_name . ' ' . $project->contractor->last_name, ['controller' => 'Contractors', 'action' => 'view', $project->contractor->id]) : '-' ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Organisation') ?></th>
+                            <td><?= $project->hasValue('organisation') ? $this->Html->link($project->organisation->business_name, ['controller' => 'Organisations', 'action' => 'view', $project->organisation->id]) : '-' ?></td>
+                        </tr>
                     </table>
                 </div>
-                <?php endif; ?>
+                <div class="col-md-6">
+                    <table class="table">
+                        <tr>
+                            <th class="w-50"><?= __('Project Due Date') ?></th>
+                            <td><?= $project->project_due_date ? h($project->project_due_date->format('Y-m-d')) : '-' ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Last Checked') ?></th>
+                            <td><?= $project->last_checked ? h($project->last_checked->format('Y-m-d')) : '-' ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Status') ?></th>
+                            <td>
+                                <?php if ($project->complete): ?>
+                                    <span class="badge bg-success text-white">Completed</span>
+                                <?php else: ?>
+                                    <span class="badge bg-warning text-dark">In Progress</span>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
+
+            <?php if ($project->description): ?>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5 class="mb-0"><?= __('Description') ?></h5>
+                    </div>
+                    <div class="card-body">
+                        <?= $this->Text->autoParagraph(h($project->description)); ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <?php if (!empty($project->skills)): ?>
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="mb-0"><?= __('Required Skills') ?></h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex flex-wrap gap-2">
+                            <?php foreach ($project->skills as $skill): ?>
+                                <span class="badge bg-primary"><?= h($skill->skill_name) ?></span>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
