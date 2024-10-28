1faca37bff5b188f03485d56af7e49f16b31f8f5
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-10-28 19:59:04 +1100

    feat: implement organisation pages

--- templates/Organisations/edit.php
+++ templates/Organisations/edit.php
@@ -1,37 +1,87 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Organisation $organisation
  */
+$this->setLayout('admin')
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $organisation->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $organisation->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Organisations'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="organisations form content">
-            <?= $this->Form->create($organisation) ?>
-            <fieldset>
-                <legend><?= __('Edit Organisation') ?></legend>
-                <?php
-                    echo $this->Form->control('business_name');
-                    echo $this->Form->control('contact_first_name');
-                    echo $this->Form->control('contact_last_name');
-                    echo $this->Form->control('contact_email');
-                    echo $this->Form->control('current_website');
-                    echo $this->Form->control('industry');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+<div class="container py-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white py-3">
+                    <h3 class="card-title text-primary mb-0 fw-bold"><?= __('Edit Organisation') ?></h3>
+                </div>
+                <div class="card-body p-4">
+                    <?= $this->Form->create($organisation, ['class' => 'd-flex flex-column gap-3']) ?>
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <?= $this->Form->control('business_name', [
+                                'class' => 'form-control',
+                                'label' => ['class' => 'form-label fw-semibold'],
+                                'placeholder' => 'Enter business name'
+                            ]) ?>
+                        </div>
+
+                        <div class="col-md-6">
+                            <?= $this->Form->control('contact_first_name', [
+                                'class' => 'form-control',
+                                'label' => ['class' => 'form-label fw-semibold'],
+                                'placeholder' => 'Enter first name'
+                            ]) ?>
+                        </div>
+
+                        <div class="col-md-6">
+                            <?= $this->Form->control('contact_last_name', [
+                                'class' => 'form-control',
+                                'label' => ['class' => 'form-label fw-semibold'],
+                                'placeholder' => 'Enter last name'
+                            ]) ?>
+                        </div>
+
+                        <div class="col-md-6">
+                            <?= $this->Form->control('contact_email', [
+                                'class' => 'form-control',
+                                'label' => ['class' => 'form-label fw-semibold'],
+                                'placeholder' => 'Enter email address'
+                            ]) ?>
+                        </div>
+
+                        <div class="col-md-6">
+                            <?= $this->Form->control('current_website', [
+                                'class' => 'form-control',
+                                'label' => ['class' => 'form-label fw-semibold'],
+                                'placeholder' => 'https://'
+                            ]) ?>
+                        </div>
+
+                        <div class="col-12">
+                            <?= $this->Form->control('industry', [
+                                'class' => 'form-control',
+                                'label' => ['class' => 'form-label fw-semibold'],
+                                'type' => 'textarea',
+                                'rows' => 4,
+                                'placeholder' => 'Describe the industry'
+                            ]) ?>
+                        </div>
+                    </div>
+
+                    <div class="pt-4 d-flex">
+                        <div>
+                            <?= $this->Html->link(__('Cancel'), ['action' => 'index'], [
+                                'class' => 'btn btn-outline-secondary me-2'
+                            ]) ?>
+                        </div>
+                        <div class="mx-3">
+                            <?= $this->Form->button(__('Update Organisation'), [
+                                'class' => 'btn btn-primary px-4',
+                            ]) ?>
+                        </div>
+                    </div>
+                    <?= $this->Form->end() ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- templates/Organisations/view.php
+++ templates/Organisations/view.php
@@ -1,130 +1,136 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Organisation $organisation
  */
+$this->setLayout('admin')
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Organisation'), ['action' => 'edit', $organisation->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Organisation'), ['action' => 'delete', $organisation->id], ['confirm' => __('Are you sure you want to delete # {0}?', $organisation->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Organisations'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Organisation'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+<div class="container py-4">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3 d-flex justify-content-between align-items-center">
+            <h3 class="m-0 font-weight-bold text-primary"><?= h($organisation->business_name) ?></h3>
+            <div>
+                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $organisation->id], ['class' => 'btn btn-secondary btn-sm me-2 px-4']) ?>
+                <?= $this->Form->postLink(
+                    __('Delete'),
+                    ['action' => 'delete', $organisation->id],
+                    ['confirm' => __('Are you sure you want to delete this organisation?'), 'class' => 'btn btn-danger btn-sm px-3']
+                ) ?>
+            </div>
         </div>
-    </aside>
-    <div class="column column-80">
-        <div class="organisations view content">
-            <h3><?= h($organisation->id) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Business Name') ?></th>
-                    <td><?= h($organisation->business_name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Contact First Name') ?></th>
-                    <td><?= h($organisation->contact_first_name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Contact Last Name') ?></th>
-                    <td><?= h($organisation->contact_last_name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Contact Email') ?></th>
-                    <td><?= h($organisation->contact_email) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Current Website') ?></th>
-                    <td><?= h($organisation->current_website) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($organisation->id) ?></td>
-                </tr>
-            </table>
-            <div class="text">
-                <strong><?= __('Industry') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($organisation->industry)); ?>
-                </blockquote>
-            </div>
-            <div class="related">
-                <h4><?= __('Related Contacts') ?></h4>
-                <?php if (!empty($organisation->contacts)) : ?>
-                <div class="table-responsive">
-                    <table>
+        <div class="card-body">
+            <div class="row mb-4">
+                <div class="col-md-6">
+                    <table class="table">
                         <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('First Name') ?></th>
-                            <th><?= __('Last Name') ?></th>
-                            <th><?= __('Email') ?></th>
-                            <th><?= __('Phone Number') ?></th>
-                            <th><?= __('Message') ?></th>
-                            <th><?= __('Organisation Id') ?></th>
-                            <th><?= __('Contractor Id') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
+                            <th class="w-50"><?= __('Business Name') ?></th>
+                            <td><?= h($organisation->business_name) ?></td>
                         </tr>
-                        <?php foreach ($organisation->contacts as $contact) : ?>
                         <tr>
-                            <td><?= h($contact->id) ?></td>
-                            <td><?= h($contact->first_name) ?></td>
-                            <td><?= h($contact->last_name) ?></td>
-                            <td><?= h($contact->email) ?></td>
-                            <td><?= h($contact->phone_number) ?></td>
-                            <td><?= h($contact->message) ?></td>
-                            <td><?= h($contact->organisation_id) ?></td>
-                            <td><?= h($contact->contractor_id) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Contacts', 'action' => 'view', $contact->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Contacts', 'action' => 'edit', $contact->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Contacts', 'action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete # {0}?', $contact->id)]) ?>
-                            </td>
+                            <th><?= __('Contact Name') ?></th>
+                            <td><?= h($organisation->contact_first_name . ' ' . $organisation->contact_last_name) ?></td>
                         </tr>
-                        <?php endforeach; ?>
+                        <tr>
+                            <th><?= __('Contact Email') ?></th>
+                            <td><?= h($organisation->contact_email) ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Current Website') ?></th>
+                            <td><?= $organisation->current_website ? $this->Html->link($organisation->current_website, $organisation->current_website, ['target' => '_blank']) : '-' ?></td>
+                        </tr>
                     </table>
                 </div>
-                <?php endif; ?>
             </div>
-            <div class="related">
-                <h4><?= __('Related Projects') ?></h4>
-                <?php if (!empty($organisation->projects)) : ?>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Project Name') ?></th>
-                            <th><?= __('Description') ?></th>
-                            <th><?= __('Management Tool Link') ?></th>
-                            <th><?= __('Project Due Date') ?></th>
-                            <th><?= __('Last Checked') ?></th>
-                            <th><?= __('Complete') ?></th>
-                            <th><?= __('Contractor Id') ?></th>
-                            <th><?= __('Organisation Id') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
-                        </tr>
-                        <?php foreach ($organisation->projects as $project) : ?>
-                        <tr>
-                            <td><?= h($project->id) ?></td>
-                            <td><?= h($project->project_name) ?></td>
-                            <td><?= h($project->description) ?></td>
-                            <td><?= h($project->management_tool_link) ?></td>
-                            <td><?= h($project->project_due_date) ?></td>
-                            <td><?= h($project->last_checked) ?></td>
-                            <td><?= h($project->complete) ?></td>
-                            <td><?= h($project->contractor_id) ?></td>
-                            <td><?= h($project->organisation_id) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Projects', 'action' => 'view', $project->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Projects', 'action' => 'edit', $project->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Projects', 'action' => 'delete', $project->id], ['confirm' => __('Are you sure you want to delete # {0}?', $project->id)]) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
+
+            <?php if ($organisation->industry): ?>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5 class="mb-0"><?= __('Industry') ?></h5>
+                    </div>
+                    <div class="card-body">
+                        <?= $this->Text->autoParagraph(h($organisation->industry)); ?>
+                    </div>
                 </div>
-                <?php endif; ?>
-            </div>
+            <?php endif; ?>
+
+            <?php if (!empty($organisation->contacts)): ?>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5 class="mb-0"><?= __('Related Contacts') ?></h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th><?= __('Name') ?></th>
+                                        <th><?= __('Email') ?></th>
+                                        <th><?= __('Phone Number') ?></th>
+                                        <th class="actions"><?= __('Actions') ?></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($organisation->contacts as $contact): ?>
+                                        <tr>
+                                            <td><?= h($contact->first_name . ' ' . $contact->last_name) ?></td>
+                                            <td><?= h($contact->email) ?></td>
+                                            <td><?= h($contact->phone_number) ?></td>
+                                            <td class="actions">
+                                                <?= $this->Html->link(__('View'), ['controller' => 'Contacts', 'action' => 'view', $contact->id], ['class' => 'btn btn-sm btn-primary me-2']) ?>
+                                                <?= $this->Html->link(__('Edit'), ['controller' => 'Contacts', 'action' => 'edit', $contact->id], ['class' => 'btn btn-sm btn-secondary me-2']) ?>
+                                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Contacts', 'action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete this contact?'), 'class' => 'btn btn-sm btn-danger']) ?>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <?php if (!empty($organisation->projects)): ?>
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="mb-0"><?= __('Related Projects') ?></h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th><?= __('Project Name') ?></th>
+                                        <th><?= __('Due Date') ?></th>
+                                        <th><?= __('Status') ?></th>
+                                        <th class="actions"><?= __('Actions') ?></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($organisation->projects as $project): ?>
+                                        <tr>
+                                            <td><?= h($project->project_name) ?></td>
+                                            <td><?= $project->project_due_date ? h($project->project_due_date->format('Y-m-d')) : '-' ?></td>
+                                            <td>
+                                                <?php if ($project->complete): ?>
+                                                    <span class="badge bg-success text-white">Completed</span>
+                                                <?php else: ?>
+                                                    <span class="badge bg-warning text-dark">In Progress</span>
+                                                <?php endif; ?>
+                                            </td>
+                                            <td class="actions">
+                                                <?= $this->Html->link(__('View'), ['controller' => 'Projects', 'action' => 'view', $project->id], ['class' => 'btn btn-sm btn-primary me-2']) ?>
+                                                <?= $this->Html->link(__('Edit'), ['controller' => 'Projects', 'action' => 'edit', $project->id], ['class' => 'btn btn-sm btn-secondary me-2']) ?>
+                                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Projects', 'action' => 'delete', $project->id], ['confirm' => __('Are you sure you want to delete this project?'), 'class' => 'btn btn-sm btn-danger']) ?>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
