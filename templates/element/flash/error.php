28cf8be98e792dd1f6407d63518c4e985f2968c8
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-10-28 16:48:24 +1100

    fix

--- templates/element/flash/error.php
+++ templates/element/flash/error.php
@@ -9,7 +9,4 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="alert alert-danger alert-dismissible fade show py-2" onclick="this.classList.add('hidden');" role="alert">
-    <?= $message ?>
-    <button type="button" class="btn-close small" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+<div class="message error" onclick="this.classList.add('hidden');"><?= $message ?></div>
