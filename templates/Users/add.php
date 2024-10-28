34e4d654feeb3118d7cc080ac04df15b7977e7c8
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-10-28 18:13:46 +1100

    feat: add stylign to register screen

--- templates/Users/add.php
+++ templates/Users/add.php
@@ -1,30 +1,81 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\User $user
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="users form content">
-            <?= $this->Form->create($user) ?>
-            <fieldset>
-                <legend><?= __('Add User') ?></legend>
-                <?php
-                    echo $this->Form->control('first_name');
-                    echo $this->Form->control('last_name');
-                    echo $this->Form->control('email');
-                    echo $this->Form->control('password');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+<div class="d-flex justify-content-center align-items-center dvh-100">
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card shadow-sm">
+                    <div class="card-header bg-white py-3">
+                        <h3 class="card-title text-center text-gray-400 mb-0 fw-bold"><?= __('Create Account') ?></h3>
+                    </div>
+                    <div class="card-body p-4">
+                        <div class="alert alert-dark mb-4" role="alert">
+                            <i class="fas fa-info-circle me-2"></i>
+                            <strong>Note:</strong> This public registration page is for demonstration purposes only.
+                        </div>
+
+                        <?= $this->Form->create($user, ['class' => 'd-flex flex-column gap-3']) ?>
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <?= $this->Form->control('first_name', [
+                                    'class' => 'form-control bold',
+                                    'label' => ['class' => 'form-label fw-semibold'],
+                                    'placeholder' => 'Enter first name'
+                                ]) ?>
+                            </div>
+
+                            <div class="col-md-6">
+                                <?= $this->Form->control('last_name', [
+                                    'class' => 'form-control bold',
+                                    'label' => ['class' => 'form-label fw-semibold'],
+                                    'placeholder' => 'Enter last name'
+                                ]) ?>
+                            </div>
+
+                            <div class="col-12">
+                                <?= $this->Form->control('email', [
+                                    'class' => 'form-control bold',
+                                    'label' => ['class' => 'form-label fw-semibold'],
+                                    'placeholder' => 'Enter your email address',
+                                    'type' => 'email'
+                                ]) ?>
+                            </div>
+
+                            <div class="col-12">
+                                <?= $this->Form->control('password', [
+                                    'class' => 'form-control bold',
+                                    'label' => ['class' => 'form-label fw-semibold'],
+                                    'placeholder' => 'Choose a strong password',
+                                    'type' => 'password'
+                                ]) ?>
+                                <div class="form-text">
+                                    Password should be at least 8 characters long and include numbers and special characters.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-grid gap-2 mt-4">
+                            <?= $this->Form->button(__('Create Account'), [
+                                'class' => 'btn btn-dark py-2',
+                            ]) ?>
+                        </div>
+
+                        <div class="text-center mt-2">
+                            Already have an account?
+                            <?= $this->Html->link(__('Sign In'), [
+                                'action' => 'login'
+                            ], ['class' => 'text-decoration-none']) ?>
+                        </div>
+                        <?= $this->Form->end() ?>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
